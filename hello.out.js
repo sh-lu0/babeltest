e1f328064cd3a8c654e0fe18966cae96159b1a62
sh-lu0 <sh-lu0@users.noreply.github.com>
2018-12-07 03:34:51 +0900

    babelrc追加

---
+++ hello.out.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// httpモジュール読み込み
+var http = require('http');
+
+// webサーバーを実行
+var svr = http.createServer(handler); //Webサーバー生成
+svr.listen(8081); //ポート8081で待ち受け開始
+
+//サーバーにアクセスされたとき
+function handler(req, res) {
+  console.log('url:', req.url);
+  console.log('method;', req.method);
+  //HTTPヘッダーを出力
+  res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+  //レスポンスの本体を出力
+  res.end('<h1>Hello,World</h1>\n');
+}
